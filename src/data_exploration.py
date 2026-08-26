cce8ef5beec853b73b701114015bf3731431180c
dantas-clara <dantas-clara@hotmail.com>
2026-08-26 19:24:10 +0100

    "add def run_exploration"

--- src/data_exploration.py
+++ src/data_exploration.py
@@ -27,3 +27,55 @@ def get_nunique(df):
 
 def get_unique(coluna):
     return coluna.unique()
+
+
+def run_exploration(df):
+    print("\n |     ISIN     |\n")
+    df_show_999 = get_999(df)
+    print(df_show_999)
+
+    # 1 todo HEAD()
+    print("\n |     HEAD     |\n")
+    df_show_head = get_head(df)
+    print(df_show_head)
+
+    # 2 todo TAIL()
+    print("\n |     TAIL     |\n")
+    df_show_tail = get_tail(df)
+    print(df_show_tail)
+
+    # 3 todo SHAPE
+    print("\n |     SHAPE     |\n")
+    df_show_shape = get_shape(df)
+    print(df_show_shape)
+
+    # 4 todo INFO
+    print("\n |     INFO     |\n")
+    df_show_info = get_info(df)
+    print(df_show_info)
+
+    # 4.1 todo ISNA
+    print("\n |     ISNA     |\n")
+    df_show_isna = get_isna(df).sum()
+    print(df_show_isna)
+
+    # 4.2 todo ISNULL PERCENTAGE
+    print("\n |     ISNULL PERCENTAGE    |\n")
+    df_show_isnull = get_missing_percentage(df)
+    print(df_show_isnull)
+
+    # 5 todo DUPLICATED
+    print("\n |     DUPLICATE     |\n")
+    df_show_duplicated = get_duplicated(df).sum()
+    print(df_show_duplicated)
+
+    # 6 todo NUNIQUE
+    print("\n |     NUNIQUE     |\n")
+    df_show_nunique = get_nunique(df)
+    print(df_show_nunique)
+
+    # 7 todo UNIQUE
+    print("\n |     UNIQUE     |\n")
+    df_show_unique = get_unique(df["Ano"])
+    print(df_show_unique)
+
