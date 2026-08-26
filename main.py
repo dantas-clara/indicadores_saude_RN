e3526700d501b70e8dd09d2082ceeeb37ed680c2
dantas-clara <dantas-clara@hotmail.com>
2026-08-26 17:26:53 +0100

    "fix functions title"

--- main.py
+++ main.py
@@ -1,41 +1,41 @@
 from src import data_loader_function
-from src import show_head, show_tail, show_shape, show_info, show_isna, show_duplicated, show_nunique
+from src import get_head, get_tail, get_shape, get_info, get_isna, get_duplicated, get_nunique
 
 df_raw = data_loader_function()
 
 #1 todo HEAD()
 print("\n |     HEAD     |\n")
-df_show_head = show_head(df_raw)
+df_show_head = get_head(df_raw)
 print(df_show_head)
 
 #2 todo TAIL()
 print("\n |     TAIL     |\n")
-df_show_tail = show_tail(df_raw)
+df_show_tail = get_tail(df_raw)
 print(df_show_tail)
 
 #3 todo SHAPE
 print("\n |     SHAPE     |\n")
-df_show_shape = show_shape(df_raw)
+df_show_shape = get_shape(df_raw)
 print(df_show_shape)
 
 #4 todo INFO
 print("\n |     INFO     |\n")
-df_show_info = show_info(df_raw)
+df_show_info = get_info(df_raw)
 print(df_show_info)
 
 #4 todo ISNA
 print("\n |     ISNA     |\n")
-df_show_isna = show_isna(df_raw).sum()
+df_show_isna = get_isna(df_raw).sum()
 print(df_show_isna)
 
 #5 todo DUPLICATED
 print("\n |     DUPLICATE     |\n")
-df_show_duplicated = show_duplicated(df_raw).sum()
+df_show_duplicated = get_duplicated(df_raw).sum()
 print(df_show_duplicated)
 
 #6 todo NUNIQUE
 print("\n |     NUNIQUE     |\n")
-df_show_nunique = show_nunique(df_raw)
+df_show_nunique = get_nunique(df_raw)
 print(df_show_nunique)
 
 
--- src/data_exploration.py
+++ src/data_exploration.py
@@ -1,21 +1,21 @@
 
-def show_head (df):
+def get_head (df):
     return df.head()
 
-def show_tail (df):
+def get_tail (df):
     return df.tail()
 
-def show_shape(df):
+def get_shape(df):
     return df.shape
 
-def show_info(df):
+def get_info(df):
     return df.info()
 
-def show_isna(df):
+def get_isna(df):
     return df.isna()
 
-def show_duplicated(df):
+def get_duplicated(df):
     return df.duplicated()
 
-def show_nunique(df):
+def get_nunique(df):
     return df.nunique()
