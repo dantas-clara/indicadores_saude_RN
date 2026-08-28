65f7b9897e0b78392d3ef91b708e5e8a57a710d5
dantas-clara <dantas-clara@hotmail.com>
2026-08-28 08:51:58 +0100

    "add df_scrubbing"

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
 from src import data_loader_function
 from src import run_exploration
+from src import run_scrubbing
 
 df_raw = data_loader_function()
 df_exploration = run_exploration(df_raw)
-
-
-
-
-
-
-
-
-
+df_scrubbing = run_scrubbing(df_raw)
 
 
 
