5114688ea93b4f9460030d9d547e57db9eeb10a2
dantas-clara <dantas-clara@hotmail.com>
2026-08-28 09:21:46 +0100

    "db_processed saved"

--- main.py
+++ main.py
@@ -7,5 +7,11 @@ df_exploration = run_exploration(df_raw)
 df_scrubbing = run_scrubbing(df_raw)
 
 
+df_processed = df_scrubbing
+df_processed.to_csv(
+    "data/processed/db_processed.csv",
+    index=False,
+    encoding="utf-8-sig"
+)
 
 
